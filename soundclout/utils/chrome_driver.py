b5284e5c29d214c9e35e9c47ee0c69d37505ee08
Matt Main_PC <matmigg0804@gmail.com>
2023-05-24 13:36:17 -0400

    making window smaller

--- soundclout/utils/chrome_driver.py
+++ soundclout/utils/chrome_driver.py
@@ -10,6 +10,7 @@ def make_chrome_options():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=900,900")
     chrome_options.add_argument(
         "--disable-software-rasterizer"
     )  # Disable software rasterizer
